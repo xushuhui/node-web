9674c0f00840ec9465960d9326112049c3a94d84
xushuhui <474497097@qq.com>
2019-11-28 09:56:31 +0800

    add validator and routes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const Koa = require("koa")
-const Router = require("koa-router")
+const parser = require('koa-bodyparser')
 const InitManager = require('./core/init')
 const app = new Koa()
-
-process.cwd()
+app.use(parser())
 InitManager.initCore(app)
 
 
